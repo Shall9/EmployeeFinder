b316f1e157377968a29e240e0e779542276f18db
Shall9 <hallshane856@gmail.com>
2018-12-28 03:59:03 -0500

    pseudocode what isn't completed yet

--- public/js/survey.js
+++ public/js/survey.js
@@ -1,3 +1,6 @@
 const $ = function (selector) {
-  
+
 }
+
+//converting each user's results into a simple array of numbers, and compare the difference between current user's score against those from other users.
+//Use absolute value of the differences
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -6,29 +6,14 @@ module.exports = (app) => {
  });
   
   app.post("/api/employees", (req, res) => {
-    
+    if (employeeList.length < 6) {
+      employeeList.push(req.body)
+    }
     
     employees.push(req.body);
     return res.json(req.body);
      
   });
-
-  
-
- 
-   
-   
-
-   
-    
-  
-
-   
-      
-   
-   
-
-   
-   
- 
+  // GET route suppose to show the survey page
+  // POST route suppose to handle incomming survey results
 };
