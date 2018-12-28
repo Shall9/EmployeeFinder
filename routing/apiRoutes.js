569f1996896b1fca9715a863959f663d5354a193
Shall9 <hallshane856@gmail.com>
2018-12-27 22:41:47 -0500

    recreating api routes and survey js files

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,26 +1,31 @@
-const employeeList = require("../app/data/employee");
+const employeeList = require("../app/data/employee.js");
 
 module.exports = (app) => {
  app.get("/api/employees", (req, res) => {
    res.json(employeeList);
  });
+  
+  app.post("/api/employees", (req, res) => {
+    employees.push(req.body);
+    return res.json(req.body);
+  });
 
- app.post("/api/employees", (req, res) => {
+  
 
-   const sum = (a, b) => Number(a) + Number(b);
-   const sumsArray = employeeList.map(({ scores }) => scores.reduce(sum));
+ 
+   
+   
 
-   let newArray = sumsArray.map( (value) => {
-       return Math.abs(value - sumsArray[sumsArray.length -1]);
-   } );
+   
+    
+  
 
-   const firstMatchIndex = newArray.slice(0, newArray.length - 1).findIndex((value, i, array) => {
-       return value === Math.min(...array);
-   });
-   const firstMatch = employeeList[firstMatchIndex];
+   
+      
+   
+   
 
-   employeeList.push(req.body);
-   return res.json(firstMatch);
-
- });
+   
+   
+ 
 };
